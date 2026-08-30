6fc74265400f52dbb797cdcc4a5fe31b1c5d0d8b
JDanielBV2806 <jhoandaniel.boterovargas@gmail.com>
2026-08-30 03:19:19 -0500

    Avance DB

--- database.js
+++ database.js
@@ -1,2 +1,11 @@
 import {CreatePool} from 'mysql2/promise';
 //recibe como parametro un objeto de js
+const pool = CreatePool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'admin',
+    database: 'Prueba01'
+});
+
+export default pool;

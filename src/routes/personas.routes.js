a60c292fbf26109998258a393adc98a38bcbd2c3
JDanielBV2806 <jhoandaniel.boterovargas@gmail.com>
2026-08-30 13:04:39 -0500

    Agregar a lista exitoso

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -6,6 +6,26 @@ const router = Router();
 router.get('/add', (req, res) => {
     res.render('personas/add');
 });
+//ruta para enviar los datos
+router.post('/add', async(req, res) => {
+   try{
+        const {nombre, apellido, edad} = req.body;
+        const newPersona = {
+            nombre,
+            apellido,
+            edad
+        };
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        //redireccione a la ruta /list para mostrar el listado de personas
+        res.redirect('/list');
+
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({message: err.message});
+    }
+});
+
 
 router.get('/list', async (req, res) => {
     try{

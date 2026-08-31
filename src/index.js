16ce458bd3ea4b47800d3bb0e539eaba86f0a6d1
JDanielBV2806 <jhoandaniel.boterovargas@gmail.com>
2026-08-30 21:14:04 -0500

    Iniciamos frontend

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import { dirname } from 'path';
@@ -5,7 +6,11 @@ import { fileURLToPath } from 'url';
 import { join } from 'path';
 import { engine } from 'express-handlebars';
 import personasRoutes from './routes/personas.routes.js';
-
+import authRoutes from './routes/auth.routes.js';
+import categoriasRoutes from './routes/categorias.routes.js';
+import productosRoutes from './routes/productos.routes.js';
+import publicoRoutes from './routes/publico.routes.js';
+import cors from 'cors';
 
 //Initializacion
 const app = express();
@@ -14,37 +19,33 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'));
-//motor de plantillas
 app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: join(app.get('views'), 'layouts'),
     partialsDir: join(app.get('views'), 'partials'),
-    //nombre del motor de plantillas
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 
-
 //middlewares
 app.use(morgan('dev'));
-//muestra el tiempo de respuesta del servidor
-app.use(express.urlencoded({extended: false}));
-//para trabajar con interfaces y formularios
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-//para trabajar con rchivos json
-
-
-
+app.use(cors());
 
 //routes
-app.get ('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.render('index');
 });
+app.use('/api/auth', authRoutes);
+app.use('/api/categorias', categoriasRoutes);
+app.use('/api/productos', productosRoutes);
+app.use('/api/publico', publicoRoutes);
 app.use(personasRoutes);
+
 //Public files
 app.use(express.static(join(__dirname, 'public')));
 
 //Run server
-app.listen(app.get('port'), () => 
-    console.log('Server listening on port', app.get('port'))); 
-
+app.listen(app.get('port'), () =>
+    console.log('Server listening on port', app.get('port')));
